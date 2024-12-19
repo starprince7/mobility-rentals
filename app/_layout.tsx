67b8636704c84f3217d3ca0ccfa7b8ece5468db3
Prince Agezi .N <princeagezinweke@gmail.com>
2024-12-20 00:17:46 +0100

    Update: Added bottom navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,17 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="(protected)/(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(protected)/(tabs)/create-listing" options={{ headerShown: false }} />
+        <Stack.Screen name="(protected)/(tabs)/profile" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </>
+  );
 }
